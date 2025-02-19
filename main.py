b03a3f7c66240c7073c7dfa2fa055665d4df7626
ry0py <ryoppy101@gmail.com>
2025-02-20 01:58:12 +0900

    写真を切り取った

--- main.py
+++ main.py
@@ -3,6 +3,11 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from PIL import Image
+import cv2
+import pytesseract
+import re
+
 
 st.title('Streamlit 超入門')
 st.write("Hello from Streamlit!")
@@ -13,3 +18,18 @@ right_col.write("Right column content")
 
 st.sidebar.header("Sidebar")
 st.sidebar.write("Sidebar content")
+# 画像をアップロードする
+uploaded_file = st.file_uploader("Choose an image...", type="jpg, png")
+if uploaded_file is not None:
+    data = np.frombuffer(uploaded_file.read(), np.uint8)
+    img = cv2.imdecode(data, cv2.IMREAD_COLOR)
+    # imgの幅と高さを取得
+    st.write("Image shape:", img.shape)
+    hand_crop = img[600:2100, 60:1130]
+    st.image(hand_crop, caption="Hand region")
+    # extracted_text = pytesseract.image_to_string(hand_crop, lang='eng')
+    # name_match = re.search(r'[A-Za-z]+', extracted_text)
+    # number_match = re.findall(r'\d+', extracted_text)
+    # st.write("Name:", name_match.group(0) if name_match else "N/A")
+    # st.write("Numbers:", number_match if number_match else "N/A")
+    # st.image(hand_crop, caption="Hand region")
